0be5ce09668f0d37f90b4478c04320e2348448b6
m.angelina <m.angellina.am@gmail.com>
2023-06-21 15:12:08 +0300

    added: back button

--- frontend/src/components/user-settings/settings/index.tsx
+++ frontend/src/components/user-settings/settings/index.tsx
@@ -10,6 +10,7 @@ import Visibility from '@mui/icons-material/Visibility'
 import VisibilityOff from '@mui/icons-material/VisibilityOff'
 import IconButton from '@mui/material/IconButton'
 import OutlinedInput from '@mui/material/OutlinedInput'
+import { BackButton } from '../../button-back'
 
 export const UserSettings = () => {
   const roles = {
@@ -68,6 +69,7 @@ export const UserSettings = () => {
 
   return (
     <>
+      <BackButton />
       <Container
         fixed
         sx={{
