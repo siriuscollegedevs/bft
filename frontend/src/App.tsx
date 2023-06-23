1c771a7f04868bee59d9bcc89ab840708697f4ad
Daniel Usov <dan.usov82@gmail.com>
2023-06-23 10:55:02 +0300

    added: base solution

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,6 +3,7 @@ import { ThemeProvider } from '@mui/material'
 import { Classic } from './styles/theme'
 import { Login } from './components/login'
 import { Main } from './components/main'
+import { ShortcutButtons } from './components/shortcut-buttons'
 
 export function App() {
   return (
@@ -12,6 +13,7 @@ export function App() {
           <Routes>
             <Route path="/login" element={<Login />} />
             <Route path="/access" element={<Main />} />
+            <Route path="/test" element={<ShortcutButtons buttonNames={['edit', 'history', 'trash']} />} />
           </Routes>
         </BrowserRouter>
       </ThemeProvider>
---
+++ frontend/src/components/shortcut-buttons/index.tsx
@@ -0,0 +1,40 @@
+import { ButtonGroup, IconButton } from '@mui/material'
+
+import { ReactComponent as EditIcon } from '../../assets/edit.svg'
+import { ReactComponent as HistoryIcon } from '../../assets/history.svg'
+import { ReactComponent as TrashIcon } from '../../assets/trash.svg'
+import { ReactComponent as CancelIcon } from '../../assets/cancel.svg'
+import { ReactComponent as ToRepayIcon } from '../../assets/toRepay.svg'
+
+type IconMapping = {
+  [key: string]: React.ReactNode
+}
+type ButtonNames = {
+  buttonNames: string[]
+}
+
+const iconMapping: IconMapping = {
+  edit: <EditIcon />,
+  history: <HistoryIcon />,
+  trash: <TrashIcon />,
+  cancel: <CancelIcon />,
+  toRepay: <ToRepayIcon />
+}
+
+const checkButtonNames = ({ buttonNames }: ButtonNames) => {
+  return buttonNames.length < 4 && buttonNames.length > 0
+}
+
+export const ShortcutButtons = ({ buttonNames }: ButtonNames) => {
+  if (!checkButtonNames({ buttonNames })) {
+    return <h6>Error</h6>
+  }
+
+  return (
+    <ButtonGroup>
+      {buttonNames.map(name => (
+        <IconButton key={name}>{iconMapping[name]}</IconButton>
+      ))}
+    </ButtonGroup>
+  )
+}
