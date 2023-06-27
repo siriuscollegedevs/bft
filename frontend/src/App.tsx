45b2404e00924a48383fb4f90d704c1c9f0e7692
m.angelina <m.angellina.am@gmail.com>
2023-06-27 17:24:08 +0300

    repair: format

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,42 +6,41 @@ import { Main } from './components/main'
 import { Settings } from './components/user-settings'
 import { MainDirectories } from './components/main-directories'
 import { FormEditDirectories } from './components/form-edit-directories'
-import { DynamicHeader } from './components/header/dynamic';
-import { StaticHeader } from './components/header/static';
-import {BackButton} from './components/button-back';
+import { DynamicHeader } from './components/header/dynamic'
+import { StaticHeader } from './components/header/static'
+import { BackButton } from './components/button-back'
 
 export function App() {
   function Header() {
-    const location = useLocation();
-    const isLoginRoute = location.pathname === '/login';
-    const isAccessRoute = location.pathname === '/access';
-    const showBackButton = !isLoginRoute && !isAccessRoute;
+    const location = useLocation()
+    const isLoginRoute = location.pathname === '/login'
+    const isAccessRoute = location.pathname === '/access'
+    const showBackButton = !isLoginRoute && !isAccessRoute
 
     return (
-        <>
-          {showBackButton && <BackButton />}
-          {isLoginRoute ? <StaticHeader /> : <DynamicHeader />}
-        </>
-    );
+      <>
+        {showBackButton && <BackButton />}
+        {isLoginRoute ? <StaticHeader /> : <DynamicHeader />}
+      </>
+    )
   }
 
-
   return (
-      <>
-        <ThemeProvider theme={Classic}>
-          <BrowserRouter>
-            <Header />
-            <Routes>
-              <Route path="/login" element={<Login />} />
-              <Route path="/access" element={<Main />} />
-              <Route path="/settings" element={<Settings />} />
-              <Route path="/directories" element={<MainDirectories />} />
-              <Route path="/accounts/:id" element={<FormEditDirectories />} />
-              <Route path="/objects/:id" element={<FormEditDirectories />} />
-              <Route path="/employees/:id" element={<FormEditDirectories />} />
-            </Routes>
-          </BrowserRouter>
-        </ThemeProvider>
-      </>
-  );
+    <>
+      <ThemeProvider theme={Classic}>
+        <BrowserRouter>
+          <Header />
+          <Routes>
+            <Route path="/login" element={<Login />} />
+            <Route path="/access" element={<Main />} />
+            <Route path="/settings" element={<Settings />} />
+            <Route path="/directories" element={<MainDirectories />} />
+            <Route path="/accounts/:id" element={<FormEditDirectories />} />
+            <Route path="/objects/:id" element={<FormEditDirectories />} />
+            <Route path="/employees/:id" element={<FormEditDirectories />} />
+          </Routes>
+        </BrowserRouter>
+      </ThemeProvider>
+    </>
+  )
 }
