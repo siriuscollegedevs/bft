654c565691a07ca2dc57f8eb33c7ab50be55a948
Daniel Usov <dan.usov82@gmail.com>
2023-06-29 22:13:03 +0300

    added: base layout

--- frontend/src/components/advanced-search/filters/index.tsx
+++ frontend/src/components/advanced-search/filters/index.tsx
@@ -3,36 +3,72 @@ import InputLabel from '@mui/material/InputLabel'
 import MenuItem from '@mui/material/MenuItem'
 import FormControl from '@mui/material/FormControl'
 import Select, { SelectChangeEvent } from '@mui/material/Select'
-import React from 'react'
 import { Container } from '@mui/system'
-import { TextField } from '@mui/material'
+import { TextField, Typography } from '@mui/material'
+import React from 'react'
+
+const directoriesProperties: { [key: string]: string } = {
+  fullName: 'ФИО',
+  role: 'Роль',
+  email: 'Электронная почта'
+}
 
 export const Filters = () => {
+  // const [age, setAge] = React.useState('')
+
+  // const handleChange = (event: SelectChangeEvent) => {
+  //   setAge(event.target.value as string)
+  // }
+
   return (
-    <Container sx={{ display: 'flex', alignItems: 'center', justifyContent: 'center', width: 845, height: 500 }}>
-      <FormControl sx={{ flexDirection: 'row' }}>
-        <InputLabel id="label">Age</InputLabel>
-        <Select
-          labelId="label"
-          id="demo-simple-select"
-          value={'Ten'}
-          label="Age"
-          sx={{ width: '280px', height: '50px' }}
-        >
-          <MenuItem value={10}>Ten</MenuItem>
-          <MenuItem value={20}>Twenty</MenuItem>
-          <MenuItem value={30}>Thirty</MenuItem>
-        </Select>
-        <TextField
-          label="Значение"
-          variant="outlined"
-          InputLabelProps={{
-            shrink: true
-          }}
-          focused
-          sx={{ marginLeft: '80px', height: '50px', width: '485px' }}
-        />
-      </FormControl>
+    <Container
+      sx={{
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        width: 845,
+        height: 500,
+        flexDirection: 'column'
+      }}
+    >
+      {Object.keys(directoriesProperties).map((property: string) => (
+        <FormControl key={property} sx={{ flexDirection: 'row' }} focused>
+          {/* <InputLabel id={`label-${property}`}>Age</InputLabel>
+          <Select
+            labelId={`label-${property}`}
+            id={`select-${property}`}
+            label="Age"
+            color="primary"
+            inputProps={{ shrink: true }}
+            sx={{
+              width: '280px',
+              height: '100%',
+              alignSelf: 'center'
+            }}
+            value={age}
+            onChange={handleChange}
+          >
+            {Object.values(directoriesProperties).map((value, index) => (
+              <MenuItem key={index} value={value}>
+                {value}
+              </MenuItem>
+            ))}
+          </Select> */}
+          <Typography variant="h6" sx={{ display: 'flex', alignItems: 'center' }}>
+            {directoriesProperties[property]}
+          </Typography>
+          <TextField
+            label={directoriesProperties[property]}
+            variant="outlined"
+            InputLabelProps={{
+              shrink: true
+            }}
+            focused
+            color="primary"
+            sx={{ marginLeft: '80px', height: '100%', width: '485px', alignSelf: 'center',marginTop: '20px'}}
+          />
+        </FormControl>
+      ))}
     </Container>
   )
 }
