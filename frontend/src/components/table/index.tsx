e27fed8502bcae4af5c773ce81c42de22c328620
Daniel Usov <dan.usov82@gmail.com>
2023-06-30 22:41:28 +0300

    added: base fork

---
+++ frontend/src/components/table/index.tsx
@@ -0,0 +1,26 @@
+import { useLocation } from 'react-router-dom'
+import { Basic } from './basic'
+import { Collapsible } from './collapsible'
+
+const basicTable = (currentURL: string) => {
+  const basicURLs = ['/objects', '/admissions']
+  return basicURLs.includes(currentURL)
+}
+
+const collapsibleTable = (currentURL: string) => {
+  const collapsibleURLs = ['/accounts', '/employees', '/admissions/']
+  return collapsibleURLs.some(url => currentURL.startsWith(url))
+}
+
+export const Table = () => {
+  const currentURL = useLocation().pathname
+
+  switch (true) {
+    case basicTable(currentURL):
+      return <Basic />
+    case collapsibleTable(currentURL):
+      return <Collapsible />
+    default:
+      return <h6>Error + {currentURL}</h6>
+  }
+}
