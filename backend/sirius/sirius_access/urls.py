7fb339c0e811cb25baccded3ea485f4f4aa81617
annashutova <annashut@inbox.ru>
2023-07-06 21:58:09 +1000

    added: authorization urls

--- backend/sirius/sirius_access/urls.py
+++ backend/sirius/sirius_access/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path, include
+from .views import *
+from rest_framework_simplejwt.views import TokenRefreshView
 
 
 urlpatterns = [
-    
+    path('login/', LoginView.as_view(), name='login'),
+    path('logout/blacklist', BlacklistTokenView.as_view(), name='blacklist'),
+    path('refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
