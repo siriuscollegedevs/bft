221f01017c1c0c187e48ce50cd19a748a764fefd
annashutova <annashut@inbox.ru>
2023-07-15 18:26:04 +1000

    added: adimin page for account

--- backend/sirius/sirius_access/admin.py
+++ backend/sirius/sirius_access/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .views import Account, AccountHistory
 
 # Register your models here.
+@admin.register(Account)
+class AccountAdmin(admin.ModelAdmin):
+    model = Account
+
+@admin.register(AccountHistory)
+class AccountHistoryAdmin(admin.ModelAdmin):
+    model = AccountHistory
