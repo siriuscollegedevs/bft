bc1122896fa3d86c620d2b70ad1f4f9febbf4303
annashutova <annashut@inbox.ru>
2023-07-19 15:25:46 +1000

    repair: url slashes

---
+++ backend/sirius/sirius/config.py
@@ -0,0 +1,12 @@
+STATUS_CHOICES = (('active', 'Активен'), ('outdated', 'Неактивен'))
+HISTORY_CHOICES = (
+    ('deleted', 'Удален'),
+    ('modified', 'Изменен'),
+    ('created', 'Создан')
+)
+
+# MODEL'S FIELDS LEN
+DEFAULT_LEN = 50
+NAMES_LEN = 20
+DEFAULT_ACTION_LEN = 10
+STATUS_LEN = 10
--- backend/sirius/sirius_access/urls.py
+++ backend/sirius/sirius_access/urls.py
@@ -9,14 +9,14 @@ urlpatterns = [
     path('object/<uuid:ObjectId>/', views.ObjectApiView.as_view()),
     path('object/history/<uuid:ObjectId>/', views.ObjectHistoryApiView.as_view()),
     # ACCOUNTS
-    path('accounts/', views.GetActualAccounts.as_view()),
-    path('accounts/archive/', views.GetArchiveAccounts.as_view()),
-    path('account/', views.PostAccount.as_view()),
-    path('account/<uuid:AccountId>/', views.GetPutDeleteAccount.as_view()),
-    path('account/change_pswd/<uuid:AccountId>/', views.ChangePasswordApi.as_view()),
-    path('account/history/<uuid:AccountId>/', views.AccountHistoryApiView.as_view()),
-    path('account/expand_search/', views.AccountExpandSearch.as_view()),
+    path('accounts', views.GetActualAccounts.as_view()),
+    path('accounts/archive', views.GetArchiveAccounts.as_view()),
+    path('account', views.PostAccount.as_view()),
+    path('account/<uuid:AccountId>', views.GetPutDeleteAccount.as_view()),
+    path('account/change_pswd/<uuid:AccountId>', views.ChangePasswordApi.as_view()),
+    path('account/history/<uuid:AccountId>', views.AccountHistoryApiView.as_view()),
+    path('account/expand_search', views.AccountExpandSearch.as_view()),
     # ACCOUNT TO OBJECTS
-    path('object/accounts/', views.GetAccountsObjectsView.as_view()),
-    path('account_to_objects/', views.GetAccountByObjectView.as_view()),
+    path('object/accounts', views.GetAccountsObjectsView.as_view()),
+    path('account_to_objects', views.GetAccountByObjectView.as_view()),
 ]
