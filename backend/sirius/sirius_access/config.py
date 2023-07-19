330cd5c14acc4c6b99ed26fcefb8f9e1a6e0eadf
annashutova <annashut@inbox.ru>
2023-07-19 15:41:26 +1000

    repair: config for accounts and objects

--- backend/sirius/sirius_access/config.py
+++ backend/sirius/sirius_access/config.py
@@ -1,9 +1,5 @@
 # fields choices
-STATUS_CHOICES = (('active', 'Активен'), ('outdated', 'Неактивен'))
-
-HISTORY_CHOICES = (('delited', 'Удален'), ('modified',
-                   'Изменен'), ('created', 'Создан'))
-ACCOUNT_HISTORY_CHOICES = (('delited', 'Удален'), ('modified',
+ACCOUNT_HISTORY_CHOICES = (('deleted', 'Удален'), ('modified',
                    'Изменен'), ('created', 'Создан'), ('password_changed', 'Изменен пароль'))
 
 TYPE_CHOICES_ACCOUNT = (
@@ -14,16 +10,13 @@ TYPE_CHOICES_ACCOUNT = (
 )
 
 # MODEL'S FIELDS LEN
-DEFAULT_LEN = 50
-NAMES_LEN = 20
 ACTION_ACCOUNT_LEN = 16
-DEFAULT_ACTION_LEN = 10
 ACCOUNT_TYPE_LEN = 16
-STATUS_LEN = 10
 
 #VALUE SETS FOR REQUESTS AND RESPONSES
 GET_ACCOUNTS_FIELDS = ('id', 'role', 'first_name', 'surname', 'last_name', 'username')
 ACCOUNT_GET_REQUEST_FIELDS = ('role', 'first_name', 'surname', 'last_name', 'username', 'password')
 GET_ACCOUNT_FIELDS = ('role', 'first_name', 'surname', 'last_name', 'username')
+GET_ACCOUNT_OBJECTS_FIELDS = ('id', 'role', 'first_name', 'surname', 'last_name', 'username', 'objects')
 
 ACCOUNT_TYPES = ('administrator', 'manager', 'specialist', 'security_officer')
