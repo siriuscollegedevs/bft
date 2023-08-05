9d86e0c539ebdf7db0dbb0169bd0c43083885e30
annashutova <annashut@inbox.ru>
2023-08-06 01:08:09 +1000

    repair: auth_cookie secure

--- backend/sirius/sirius/settings.py
+++ backend/sirius/sirius/settings.py
@@ -104,7 +104,7 @@ SIMPLE_JWT = {
     "AUTH_COOKIE_ACCESS": "access",
     "AUTH_COOKIE_REFRESH": "refresh",
     "AUTH_COOKIE_DOMAIN": None,
-    "AUTH_COOKIE_SECURE": False,
+    "AUTH_COOKIE_SECURE": True,
     "AUTH_COOKIE_HTTP_ONLY": True,
     "AUTH_COOKIE_PATH": "/",
     "AUTH_COOKIE_SAMESITE": None,
