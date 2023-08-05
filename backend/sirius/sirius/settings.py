c61b65c658d14f57ed66d15d37f5d97dcacae88f
annashutova <annashut@inbox.ru>
2023-08-06 01:08:09 +1000

    repair: auth cookie none

--- backend/sirius/sirius/settings.py
+++ backend/sirius/sirius/settings.py
@@ -107,7 +107,7 @@ SIMPLE_JWT = {
     "AUTH_COOKIE_SECURE": True,
     "AUTH_COOKIE_HTTP_ONLY": True,
     "AUTH_COOKIE_PATH": "/",
-    "AUTH_COOKIE_SAMESITE": None,
+    "AUTH_COOKIE_SAMESITE": 'None',
 }
 
 ROOT_URLCONF = 'sirius.urls'
