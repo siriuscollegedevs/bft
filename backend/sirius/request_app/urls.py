2efaca548777d07ad9595a52837ddd42fc011ba8
annashutova <annashut@inbox.ru>
2023-08-07 20:00:37 +1000

    repair: get all requests url

--- backend/sirius/request_app/urls.py
+++ backend/sirius/request_app/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     path('create', views.PostRequest.as_view()),
     path('<uuid:RequestId>', views.RequestApiView.as_view()),
-    path('all', views.GetActiveRequests.as_view()),
+    path('requests', views.GetActiveRequests.as_view()),
     path('archive', views.GetArchiveRequests.as_view()),
     path('change_status/<uuid:RequestId>', views.ChangeStatusRequest.as_view()),
     path('record/human/<uuid:RequestId>', views.HumanRecord.as_view()),
