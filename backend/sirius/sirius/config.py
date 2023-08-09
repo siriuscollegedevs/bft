6faf39df6c50908b270844903fc63c3f77ac0d49
annashutova <annashut@inbox.ru>
2023-08-10 02:38:30 +1000

    added: global error messages

--- backend/sirius/sirius/config.py
+++ backend/sirius/sirius/config.py
@@ -13,3 +13,7 @@ STATUS_LEN = 10
 
 USELESS_FIELDS = ['_state', 'id']
 FIELDS_TO_ADD =  ['timestamp', 'action', 'modified_by_id']
+
+# ERROR MESSAGES
+DB_ERROR = error = {"error": "Ошибка базы данных"}
+NO_DATA_FOUND_ERROR = {"error": "Данные не найдены"}
