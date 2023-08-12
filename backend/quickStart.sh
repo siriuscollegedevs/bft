ecf7733b44b661c5253c774fbe3969347b24a7f1
Daniel Usov <dan.usov82@gmail.com>
2023-08-12 12:10:53 +0300

    added: autorun script

---
+++ backend/quickStart.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+python3 -m venv ./venv
+
+source ./venv/bin/activate
+
+pip install -r requirements.txt
+
+cd sirius
+
+migration_output=$(python manage.py migrate 2>&1)
+
+if [[ $migration_output == *"No migrations to apply."* ]]; then
+    
+    echo -e "\nОшибка при выполнении миграций. Выполняю reset_db -c...\n"
+    echo "yes" | python manage.py reset_db -c
+    echo -e "\nВыполняю миграции снова...\n"
+    python manage.py migrate
+else
+    echo -e "\nМиграции выполнены успешно\n"
+fi
+
+python manage.py runsslserver
