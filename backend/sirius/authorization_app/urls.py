50b91f9201a679133c42c5bd8d2280924d4461df
annashutova <annashut@inbox.ru>
2023-08-22 17:16:31 +1000

    added: url for clear cookie

--- backend/sirius/authorization_app/urls.py
+++ backend/sirius/authorization_app/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import LoginView, LogoutView, CookieTokenRefreshView
+from .views import LoginView, LogoutView, CookieTokenRefreshView, ClearCookie
 
 
 urlpatterns = [
     path('login', LoginView.as_view(), name='login'),
     path('logout', LogoutView.as_view(), name='logout'),
     path('refresh', CookieTokenRefreshView.as_view(), name='token_refresh'),
+    path('clear', ClearCookie.as_view(), name='clear_cookie')
 ]
