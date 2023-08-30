820ffe1c90075d71d51c6cefb15361f9a0a48dfb
annashutova <annashut@inbox.ru>
2023-08-30 14:23:45 +0300

    repair: url conflict

--- backend/sirius/sirius_access/urls.py
+++ backend/sirius/sirius_access/urls.py
@@ -3,14 +3,14 @@ from . import views
 
 urlpatterns = [
     # OBJECTS
-    path('objects/archive', views.GetArchiveObjects.as_view()),
-    path('objects', views.GetActualObjects.as_view()),
+    path('objects/all/archive', views.GetArchiveObjects.as_view()),
+    path('objects/all', views.GetActualObjects.as_view()),
     path('object', views.PostObject.as_view()),
     path('object/<uuid:ObjectId>', views.ObjectApiView.as_view()),
     path('object/history/<uuid:ObjectId>', views.ObjectHistoryApiView.as_view()),
     # ACCOUNTS
-    path('accounts', views.GetActualAccounts.as_view()),
-    path('accounts/archive', views.GetArchiveAccounts.as_view()),
+    path('accounts/all', views.GetActualAccounts.as_view()),
+    path('accounts/all/archive', views.GetArchiveAccounts.as_view()),
     path('account', views.PostAccount.as_view()),
     path('account/<uuid:AccountId>', views.GetPutDeleteAccount.as_view()),
     path('account/change_pswd/<uuid:AccountId>', views.ChangePasswordApi.as_view()),
